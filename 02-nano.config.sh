21ffd5c535b7f201372d64d919d696901ac642c0
BrunoCLPinto <brunocl.pinto@gmail.com>
2020-01-07 08:59:28 +0000

    Jupyter at boot correction

--- 02-nano.config.sh
+++ 02-nano.config.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
-jupyter labextension install @jupyterlab/statusbar
 jupyter lab --generate-config
-mv jetsonNanoConfig/jupyter_notebook_config.py .jupyter/jupyter_notebook_config.py
-
-# make it start at boot
-#sudo echo 'su bp -c "jupyter notebook --config=~/.jupyter/jupyter_notebook_config.py --no-browser --notebook-dir=~/works" &' >> /etc/rc.local
-#sudo chmod +x /etc/rc.local
-jupyter notebook password
+cp jetsonNanoConfig/jupyter_notebook_config.py .jupyter/jupyter_notebook_config.py
 
 # this is for my work env, remove if not applicable
+git config --global credential.helper store
 mkdir works
 cd works
-git clone https://github.com/fastai/fastai.git
+git clone https://github.com/fastai/course-v3.git
+git clone https://github.com/brunoclpinto/Fastai.jetsonNano.git
+git clone https://github.com/brunoclpinto/euromillions.git
 cd ..
+
+# make it start at boot
+sudo /bin/bash -c 'echo "su bp -c \"/home/bp/.local/bin/jupyter notebook --config=/home/bp/.jupyter/jupyter_notebook_config.py --no-browser --notebook-dir=/home/bp/works\" &" >> /etc/rc.local'
+sudo chmod +x /etc/rc.local
+jupyter notebook password
 
 sudo reboot
