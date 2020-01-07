9c74e012792682cb7553e6865ee69be3e2341fe5
BrunoCLPinto <brunocl.pinto@gmail.com>
2020-01-07 19:56:59 +0000

    Config

--- 02-nano.config.sh
+++ 02-nano.config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
-jupyter lab --generate-config
+sudo jupyter labextension install @jupyter-widgets/jupyterlab-manager
+sudo jupyter lab --generate-config
+mkdir .jupyter
 cp jetsonNanoConfig/jupyter_notebook_config.py .jupyter/jupyter_notebook_config.py
 
 # this is for my work env, remove if not applicable
@@ -13,7 +14,7 @@ git clone https://github.com/brunoclpinto/euromillions.git
 cd ..
 
 # make it start at boot
-sudo /bin/bash -c 'echo "su bp -c \"/home/bp/.local/bin/jupyter notebook --config=/home/bp/.jupyter/jupyter_notebook_config.py --no-browser --notebook-dir=/home/bp/works\" &" >> /etc/rc.local'
+sudo /bin/bash -c 'echo "su bp -c \"/usr/local/bin/jupyter notebook --config=/home/bp/.jupyter/jupyter_notebook_config.py --no-browser --notebook-dir=/home/bp/works\" &" >> /etc/rc.local'
 sudo chmod +x /etc/rc.local
 jupyter notebook password
 
